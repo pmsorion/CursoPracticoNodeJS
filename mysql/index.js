ddf8211fcca909c23b65fbf88a705deafeef1244
Francisco Garcia <garciafran@hotmail.com>
2020-06-12 18:26:22 -0500

    Separando la base de datos a un MicroServicio

--- mysql/index.js
+++ mysql/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const config = require('../config.js');
+const router = require('./network.js');
 
 const app = express();
 
 app.use(bodyParser.json());
 
 //Routes
+
+app.use('/', router)
 
 
 app.listen(config.mysqlService.port, () => {
