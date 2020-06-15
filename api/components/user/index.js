b9715c97af49587b57da09d5e41ac7e5932c1bea
Francisco Garcia <garciafran@hotmail.com>
2020-06-14 22:43:50 -0500

    Microservicios en vercel Now y serverles y seguridad

--- api/components/user/index.js
+++ api/components/user/index.js
@@ -1,5 +1,15 @@
 //const store = require('../../../store/mysql');
-const store = require('../../../store/remote-mysql');
+const config = require('../../../config');
+
+let store;
+
+if (config.remoteDB === true) {
+    store = require('../../../store/remote-mysql');
+} else {
+    store = require('../../../store/mysql');
+}
+
 const ctrl = require('./controller');
+//const { remoteDB } = require('../../../config');
 
 module.exports = ctrl(store);
--- config.js
+++ config.js
@@ -1,6 +1,10 @@
 module.exports = {
+    remoteDB: process.env.REMOTE_DB || false,
     api: {
         port: process.env.API_PORT || 3000,
+    },
+    post: {
+        port: process.env.POST_PORT || 3002,
     },
     jwt: {
         secret: process.env.JWT_SECRET || 'notasecret!', 
